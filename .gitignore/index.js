0003b876d2b276f62a3a348fd3244694a57c9b30
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-09 19:57:27 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -24,7 +24,7 @@ client.on("message", message => {
     if (message.content.startsWith(prefix +'setmoney')) {
         if (message.author.id !== admin) {
             var usereco = message.mentions.members.first();
-            var etape1 = message.content.substring(usereco);
+            var etape1 = message.content.substring(11, usereco);
             var moneyset = message.content.substring(0, usereco);
             console.log(etape1);
             console.log(moneyset);
