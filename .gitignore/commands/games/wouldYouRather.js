5455bf96c530a57bc05a8a2b3f125d523b053e0d
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-09 16:21:43 +0100

    Update wouldYouRather.js

--- .gitignore/commands/games/wouldYouRather.js
+++ .gitignore/commands/games/wouldYouRather.js
@@ -1,1 +1,21 @@
+const question = xrequire('./assets/wouldYouRather.json');
 
+module.exports = class wouldYouRather extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'wouldyourather',
+			aliases: ['wyr', 'wouldyou'],
+			group: 'games',
+			memberName: 'wouldyourather',
+			description: 'Montre une situation que vous préférez. Voyez comment vous et vos amis répondez à cette question !',
+			clientPermissions: ['EMBED_LINKS']
+		});
+
+    async (Bastion, message) => {
+    await message.channel.send({
+    embed: {
+      color: Bastion.colors.BLUE,
+      description: question[Math.floor(Math.random() * question.length)]
+    }
+  });
+};
