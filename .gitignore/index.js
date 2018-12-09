da2f7f312a5c9843cf2fec6e3779c62a5f73b1bb
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-09 15:50:04 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,16 +13,14 @@ client.registry
   .registerDefaults()
   .registerCommandsIn(__dirname+"/commands");
 
-var prefix = "o!";
-
 client.on("ready", () => {
 var memberCount = client.users.size;
 var servercount = client.guilds.size;
-    client.user.setGame('/help')
-    client.user.setAvatar('./avatar.jpg')
+    client.user.setActivity('/help')
+    //client.user.setAvatar('./avatar.jpg')
     var servers = client.guilds.array().map(g => g.name).join(',');
     console.log("--------------------------------------");
-console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+console.log("[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle: o! \n[!]Mentions = <@521330981144100864> \n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
 });
 
 client.login(process.env.TOKEN);
