d75ad2ed460f97dd725966dbcc8c9dad12048b88
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-09 19:26:15 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const quickdb = require('quick.db');
 const blacklist = new quickdb.table('blacklist');
-const eco = require("discord-economy);
+const eco = require("discord-economy");
 
 const prefix = "o!"; 
 const time = "1000"; 
