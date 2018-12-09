df8db553dca8729f383fcbb9a973a19b5edb1701
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-09 16:55:09 +0100

    Update wouldYouRather.js

--- .gitignore/commands/games/wouldYouRather.js
+++ .gitignore/commands/games/wouldYouRather.js
@@ -10,13 +10,13 @@ module.exports = class wouldYouRather extends Command {
 			description: 'Montre une situation que vous préférez. Voyez comment vous et vos amis répondez à cette question !',
 			clientPermissions: ['EMBED_LINKS']
 		});
-	};
+	}
 
-exports.exec = async (OneBot, message) => {
-    await message.channel.send({
-    embed: {
-      color: OneBot.colors.BLUE,
-      description: question[Math.floor(Math.random() * question.length)]
-    }
-  });
+        async run(message) {
+             await message.channel.send({
+                embed: {
+                  color: OneBot.colors.BLUE,
+                  description: question[Math.floor(Math.random() * question.length)]
+                }
+             });
 };
