e4ad00a0211a265a7f1b0e1eeff478f8110661a8
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-09 17:05:10 +0100

    Update wouldYouRather.js

--- .gitignore/commands/games/wouldYouRather.js
+++ .gitignore/commands/games/wouldYouRather.js
@@ -1,23 +1,25 @@
 const question = xrequire('./assets/wouldYouRather.json');
 
-module.exports = class wouldYouRather extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'wouldyourather',
-			aliases: ['wyr', 'wouldyou'],
-			group: 'games',
-			memberName: 'wouldyourather',
-			description: 'Montre une situation que vous préférez. Voyez comment vous et vos amis répondez à cette question !',
-			clientPermissions: ['EMBED_LINKS']
-		});
-	}
+exports.exec = async (OneBot, message) => {
+  await message.channel.send({
+    embed: {
+      color: OneBot.colors.BLUE,
+      description: question[Math.floor(Math.random() * question.length)]
+    }
+  });
+};
 
-        async run(message) {
-             await message.channel.send({
-                embed: {
-                  color: OneBot.colors.BLUE,
-                  description: question[Math.floor(Math.random() * question.length)]
-                }
-             });
-        }
-}
+exports.config = {
+  aliases: [ 'wouldyou' ],
+  enabled: true
+};
+
+exports.help = {
+  name: 'wouldYouRather',
+  description: 'Shows a would you rather situation. See how you and your friends answer that!',
+  botPermission: '',
+  userTextPermission: '',
+  userVoicePermission: '',
+  usage: 'wouldYouRather',
+  example: []
+};
