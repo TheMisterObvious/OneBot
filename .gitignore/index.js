a053549feca70367ec7c93e5fce798c9149b4f8d
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-09 17:39:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,4 @@
 const Commando = require('discord.js-commando');
-
 const client = new Commando.Client({
   owner: "338339839617269762",
   commandPrefix: "o!"
@@ -25,6 +24,92 @@ var servercount = client.guilds.size;
     var servers = client.guilds.array().map(g => g.name).join(',');
     console.log("--------------------------------------");
 console.log("[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle: o! \n[!]Mentions = <@521330981144100864> \n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+});
+
+//Gban
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const quickdb = require('quick.db');
+const blacklist = new quickdb.table('blacklist');
+
+const prefix = "o!"; 
+const time = "1000"; 
+const admin = "338339839617269762";
+
+setInterval(function(){
+       var nombreMembresBannis = 0;
+       var membresBannis = [];
+       var blacklisted = blacklist.get('blacklist.users');
+        
+client.guilds.forEach(serveur => {
+       serveur.members.forEach(membre => {
+            blacklisted.forEach(user =>{
+                    if(membre.id === user){
+                        membre.send('Vous avez été banni de **'+serveur+'** car vous faites parti de la blacklist !');
+                        membre.ban('utilisateur dans la blacklist !');
+                        banni++;
+                        membresBannis.push(membre);
+                    }
+                });
+            });
+        });
+    }, time);
+});
+
+
+client.on("message", async message => {
+       if(message.content.indexOf(prefix !== 0)) return;
+  
+       const args = message.content.slice(prefix.length).trim().split(/ +/g);
+       const command = args.shift().toLowerCase();
+       
+       if(command === 'gban'){
+            if(message.author.id !== admin) return message.reply('permissions insuffisantes');
+                    
+            var to_add = args[0];
+         
+            if(!to_add) return message.reply('entre une ID à blacklist !');
+            if(isNaN(to_add)) return message.reply('entre une ID valide !');
+        
+            var validUser = false;
+        
+            client.fetchUser(to_add).then(the_user =>{
+                 validUser = the_user;
+            }).catch(err => {
+            return message.reply('aucun utilisateur avec une telle ID trouvé !');
+        });
+        if(validUser){
+            var blacklisted = blacklist.get('blacklist.users');
+            if(blacklisted.includes(validUser.id)) return message.reply('ID déjà enregistrée !');
+            else blacklist.push('blacklist.users', validUser.id);
+            return message.reply('utilisateur **'+validUser.username+'#'+validUser.discriminator+'** blacklisté !');
+        }
+    }
+    if(command === 'rgban'){
+        if(message.author.id !== admin) return message.reply('permissions insuffisantes');
+        var to_remove = args[0];
+        if(!to_remove) return message.reply('entre une ID à unblacklist !');
+        if(isNaN(to_remove)) return message.reply('entre une ID valide !');
+        var validUser = false;
+        client.fetchUser(to_remove).then(the_user =>{
+            validUser = the_user;
+        }).catch(err => {
+            return message.reply('aucun utilisateur avec une telle ID trouvé !');
+        });
+        if(validUser){
+            var blacklisted = blacklist.get('blacklist.users');
+            if(!blacklisted.includes(validUser.id)) return message.reply('l\'ID n\'est pas blacklist !');
+            else var all_members = [];
+            blacklisted.forEach(element => {
+                if(element !== validUser.id){
+                    all_members.push(element);
+                }
+            });
+            blacklist.set('blacklist.users', all_members);
+            return message.reply('utilisateur **'+validUser.username+'#'+validUser.discriminator+'** unblacklist ! !');
+        }
+    }
 });
 
 client.login(process.env.TOKEN);
