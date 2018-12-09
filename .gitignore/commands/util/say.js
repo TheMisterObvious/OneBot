e2f4ea6cdf568cfc40f2a563b80130c70c46abd0
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-09 14:21:47 +0100

    Update say.js

--- .gitignore/commands/util/say.js
+++ .gitignore/commands/util/say.js
@@ -1,1 +1,21 @@
+const Commando = require('discord.js-commando');
 
+class SayCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'say',
+      group: 'chattools',
+      memberName: 'say',
+      description: 'Simon says.',
+      format: 'text',
+    });
+  }
+
+  async run(message, args) {
+    // Delete user's comment, replace it with bot's.
+    message.delete();
+    message.say(args);
+  }
+}
+
+module.exports = SayCommand;
