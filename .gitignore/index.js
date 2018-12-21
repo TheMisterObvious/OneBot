e3781aca7c4429d294dd02ca874e8cbd91a2b1fe
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2018-12-21 22:29:55 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const quickdb = require('quick.db');
 const blacklist = new quickdb.table('blacklist');
-const Economy = require("discordeco.js");
-const eco = new Economy('1000');
+const Economy = require("dinero.js");
+const eco = Economy
 
 const prefix = "o!"; 
 const time = "1000"; 
@@ -22,13 +22,99 @@ console.log("[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les éveneme
 });
 
 //Economie
+
+const discord = require('discord.js');
+const dl = require('discord-leveling');
+
+client.on('message', async message => {
  
-eco.fetchBalance('userID').then(money => {
-    console.log(money);
-});
+  const settings = {
+    prefix: '!',
+  }
  
-eco.updateBalance('userID', 5000).then(newBalance => {
-    console.log(newBalance);
+  var command = message.content.toLowerCase().slice(settings.prefix.length).split(' ')[0];
+  var args = message.content.split(' ').slice(1);
+ 
+  if (message.author.bot) return;
+ 
+  var profile = await dl.Fetch(message.author.id)
+  dl.AddXp(message.author.id, 10)
+  if (profile.xp + 10 > 100) {
+    await dl.AddLevel(message.author.id, 1)
+    message.reply(`You just leveled up!! You are now level: ${profile.level + 1}`)
+  }
+ 
+  if (!message.content.startsWith(tokens.prefix)) return;
+ 
+  if (command === 'profile') {
+ 
+    var user = message.mentions.users.first() || message.author
+ 
+    var output = await dl.Fetch(user.id)
+    message.channel.send(`Hey ${user.tag}! You have ${output.level} level(s)! and ${output.xp} xp!`);
+  }
+ 
+  if (command === 'setxp') {
+ 
+    var amount = args[0]
+    var user = message.mentions.users.first() || message.author
+ 
+    var output = await dl.SetXp(user.id, amount)
+    message.channel.send(`Hey ${user.tag}! You now have ${amount} xp!`);
+  }
+ 
+  if (command === 'setlevel') {
+ 
+    var amount = args[0]
+    var user = message.mentions.users.first() || message.author
+ 
+    var output = await dl.SetLevel(user.id, amount)
+    message.channel.send(`Hey ${user.tag}! You now have ${amount} levels!`);
+  }
+ 
+  if (command === 'leaderboard') {
+ 
+    if (message.mentions.users.first()) {
+ 
+      var output = await dl.Leaderboard({
+        search: message.mentions.users.first().id
+      })
+      message.channel.send(`The user ${message.mentions.users.first().tag} is number ${output.placement} on my leaderboard!`);
+ 
+    } else {
+ 
+      dl.Leaderboard({
+        limit: 3
+      }).then(async users => {
+ 
+        var firstplace = await client.fetchUser(users[0].userid)
+        var secondplace = await client.fetchUser(users[1].userid)
+        var thirdplace = await client.fetchUser(users[2].userid)
+        message.channel.send(`My leaderboard:
+ 
+1 - ${firstplace.tag} : ${users[0].level} : ${users[0].xp}
+2 - ${secondplace.tag} : ${users[1].level} : ${users[1].xp}
+3 - ${thirdplace.tag} : ${users[2].level} : ${users[2].xp}`)
+ 
+      })
+ 
+    }
+  }
+ 
+  if (command == 'delete') {
+ 
+    var user = message.mentions.users.first()
+    if (!user) return message.reply('Pls, Specify a user I have to delete in my database!')
+ 
+    if (!message.guild.me.hasPermission(`ADMINISTRATION`)) return message.reply('You need to be admin to execute this command!')
+ 
+    var output = await dl.Delete(user.id)
+    if (output.deleted == true) return message.reply('Succesfully deleted the user out of the database!')
+ 
+    message.reply('Error: Could not find the user in database.')
+ 
+  }
+ 
 });
 
 //Gban
