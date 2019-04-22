b52d1c42efe98cf09d8d3c392c15eb10ce054a8e
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-04-22 15:37:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const db = require('quick.db');
 const blacklist = new db.table('blacklist');
-const fs = require("fs");
 const moment = require("moment");
 const weather = require("weather-js");
 
@@ -191,45 +190,6 @@ client.on("message", message => {
 
 
 //Level
-
-/*
-let ldb = JSON.parse(fs.readFileSync("./levels.json", "utf8"));
-
-client.on("message", message => {
-    if (message.author.bot) return;
-    if (!ldb[message.author.id]) ldb[message.author.id] = {
-        xp: 0,
-        level: 0
-      };
-    ldb[message.author.id].xp++;
-    let userInfo = ldb[message.author.id];
-    if(userInfo.xp > 100) {
-        userInfo.level++
-        userInfo.xp = 0
-        message.reply("Bravo, vous avez passer un niveau !")
-    }
-    const largs = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const cmd = largs.shift().toLowerCase();
-    if(cmd === "level") {
-        let userInfo = ldb[message.author.id];
-        let member = message.mentions.members.first();
-        let embed = new Discord.RichEmbed()
-        .setColor(0x4286f4)
-        .addField("__Level :__ ", userInfo.level)
-        .addField("__XP :__ ", userInfo.xp+"/100");
-        if(!member) return message.channel.sendEmbed(embed)
-        let memberInfo = ldb[member.id]
-        let embed2 = new Discord.RichEmbed()
-        .setColor(0x4286f4)
-        .addField("__Level :__ ", memberInfo.level)
-        .addField("__XP :__ ", memberInfo.xp+"/100")
-        message.channel.sendEmbed(embed2)
-    }
-    fs.writeFile("./json/database.json", JSON.stringify(ldb), (x) => {
-        if (x) console.error(x)
-      });
-});
-*/
 
 //Ticket
 
