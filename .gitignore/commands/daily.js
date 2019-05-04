e5f0ff6c03619e53b9655f610a5a4c459cd1be9a
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-05-04 22:05:53 +0200

    Update daily.js

--- .gitignore/commands/daily.js
+++ .gitignore/commands/daily.js
@@ -1,1 +1,26 @@
+module.exports.run = async (client, message, args) => {
 
+    const db = require("quick.db");
+    const ms = require("parse-ms");
+    
+    let timeout = 86400000;
+    let amount = Math.floor(Math.random() * 1000) + 1;
+    let daily = await db.get(`daily_${message.author.id}`);
+  
+    if (daily !== null && timeout - (Date.now() - daily) > 0) {
+        let time = ms(timeout - (Date.now() - daily));
+        message.channel.send(`Vous avez déjà récupéré votre récompense du jour, vous pourrez de nouveau la récupéré dans ${time.hours}h ${time.minutes}m ${time.seconds}s !`);
+    } else {
+        let embed = new Discord.RichEmbed()
+        .setColor("#5599ff")
+        .setDescription("Daily")
+        .addField("Vous avez reçu ", amount)
+        message.channel.send(embed);
+        db.add(`money_${message.author.id}`, amount);
+        db.set(`daily_${message.author.id}`, Date.now())
+
+}
+    
+module.exports.config = {
+        command: "daily"
+}
