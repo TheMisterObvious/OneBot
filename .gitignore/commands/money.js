b1ad0957c1fc9ab25a95dedb22a4acb1bf5ee585
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-05-04 20:35:26 +0200

    Update money.js

--- .gitignore/commands/money.js
+++ .gitignore/commands/money.js
@@ -2,10 +2,18 @@ module.exports.run = async (client, message, args) => {
 
     const db = require("quick.db");
 
-    var user = message.mentions.users.first() || message.author
+    if (args[1] === undefined) {
+        var user = message.author
+    } else if (args[1] === message.mentions.users.first()) {
+        var user === message.mentions.users.first();
+    }
     var bal = db.get(`money_${message.guild.id}_${user}`);
     if (bal === null) bal =0;
-    message.channel.send({embed:{description:"Vous avez "+ bal + " $"}});
+    if (user === message.author) {
+        message.channel.send({embed:{description:"Vous avez "+ bal +" $"}});
+    } else if (user === message.mentions.users.first()) {
+        message.channel.send({embed:{description:`${user} a `+ bal +" $"}});
+    }
 
 }
     
