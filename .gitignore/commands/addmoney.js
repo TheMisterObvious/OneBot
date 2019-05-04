8f7835ca4fb16d5e10a2d1ef258213f530765464
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-05-04 23:20:36 +0200

    Update addmoney.js

--- .gitignore/commands/addmoney.js
+++ .gitignore/commands/addmoney.js
@@ -6,17 +6,31 @@ module.exports.run = async (client, message, args) => {
     var user = message.author;
     
     if (!user.hasPermission("ADMINISTRATOR")) {
-        message.channel.send("**Vous n'avez pas la permission de faire ça (permission manquante: ADMINISTRATEUR) !**").then(msg => {
-            setTimeout(() => {
-                msg.delete()
-            }, 2500);
-        })
+        message.channel.send("**Vous n'avez pas la permission de faire ça (permission manquante: ADMINISTRATEUR) !**")
     }
     
     if (args[0] === undefined) {
         message.channel.send("**Merci de saisir une valeur à ajouter !**");
     }
     
+    if (isNaN(args[0])) {
+        message.channel.send("**Merci de saisir une valeur numérique valide ("+ args[0] +" <- ceci n'est pas une valeur numérique valide) !**");
+    }
+    
+    if (args[1] === undefined) {
+        message.channel.send("**Merci de saisir un utilisateur à qui ajouter de l'argent !**");
+    }
+    
+    if (!args[1] === mentionned) {
+        message.channel.send("**Merci de saisir un utilisateur valide !");
+    }
+    
+    let embed = new Discord.RichEmbed
+    .setColor("#5599ff")
+    .setAuthor("Addmoney")
+    .setDescription("Vous venez d'ajouter "+ args[0] +"$ à "+ mentionned)
+    message.channel.send(embed);
+    db.add(`money_${message.guild.id}_${mentionned.id}`, args[0])
 
 }
     
