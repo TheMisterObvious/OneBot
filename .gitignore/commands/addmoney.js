60c7aaf5a9c6dc801e6fcc861a2c93187712cd9b
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-05-04 23:29:46 +0200

    Update addmoney.js

--- .gitignore/commands/addmoney.js
+++ .gitignore/commands/addmoney.js
@@ -3,9 +3,8 @@ module.exports.run = async (client, message, args) => {
     const Discord = require("discord.js");
     const db = require("quick.db");
     var mentionned = message.mentions.users.first();
-    var user = message.author;
     
-    if (!user.hasPermission("ADMINISTRATOR")) {
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
         message.channel.send("**Vous n'avez pas la permission de faire ça (permission manquante: ADMINISTRATEUR) !**")
     }
     
