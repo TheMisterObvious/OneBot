5ca53c020f95a475ee938cc2bd27fcf06d2b57b9
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-05-04 22:52:03 +0200

    Update money.js

--- .gitignore/commands/money.js
+++ .gitignore/commands/money.js
@@ -11,9 +11,9 @@ module.exports.run = async (client, message, args) => {
     var bal = db.get(`money_${message.guild.id}_${user.id}`);
     if (bal === null) bal = 0;
     if (mentionned) {
-        message.channel.send({embed:{description:`${user.id} a `+ bal +" $"}});
+        message.channel.send({embed:{description:`${user.id} a `+ bal +" $", color: "#5599ff"}});
     } else {
-        message.channel.send({embed:{description:"Vous avez "+ bal +" $"}});
+        message.channel.send({embed:{description:"Vous avez "+ bal +" $", color: "#5599ff"}});
     }
 
 }
