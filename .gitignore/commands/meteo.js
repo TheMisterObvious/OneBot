ba73e16057963be69444e1797cc12eb23de0b1f5
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-05-05 19:56:25 +0200

    Update meteo.js

--- .gitignore/commands/meteo.js
+++ .gitignore/commands/meteo.js
@@ -13,11 +13,18 @@ module.exports.run = async (client, message, args) => {
                 message.channel.send("**Je n'ai pas trouvé d'information pour la méteo de "+ location +" !**");
             } else {
                 data = data[0];
+                if (data.current.skytext === "Sunny") {
+                    var skytext = "☀️";
+                } else if (data.current.skytext ==== "Mostly Cloudy") {
+                    var skytext = "";
+                } else if (data.current.skytext === "Light Rain") {
+                    var skytext = "☀️";
+                }
                 const embed = new Discord.RichEmbed()
                 .setAuthor("Météo de "+ data.location.name)
                 .addField("Maintenant :", data.current.temperature +"°"+ unit)
-                .addField("Skytext :", data.current.skytext +" ressentie")
-                .addField("Feelslike :", data.current.feelslike)
+                .addField("Ressentie :", data.current.feelslike)
+                .addField("Ciel :", skytext)
                 .addField("WindDisplay :", data.current.winddisplay + " Vent")
                 message.channel.send(embed);
                 //Prévisions pour demain : **\nHaut: " + data.forecast[1].high + "°, Bas: " + data.forecast[1].low + "° " + data.forecast[1].skytextday + " avec " + data.forecast[1].precip + "% de chance de precipitation.");
