97bb1d4b75517e744179fb6338fc25ec3374a63c
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2019-05-06 19:47:24 +0200

    Create gban.js

---
+++ .gitignore/commands/gban.js
@@ -0,0 +1,12 @@
+module.exports.run = async (client, message, args) => {
+
+    const Discord = require("discord.js");
+    const db = ("quick.db");
+    
+    //Work In Progress
+
+}
+    
+module.exports.config = {
+        command: "gban"
+}
