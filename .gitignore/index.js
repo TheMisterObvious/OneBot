856d47837a8a9bb906d22b576720e5daa10ef863
TheMisterObvious <39840495+TheMisterObvious@users.noreply.github.com>
2020-01-02 23:57:26 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -40,6 +40,10 @@ client.on("message", message => {
       db.set(`power_${user.id}`, 0);
   }
 
+if (message.content.startsWith(prefix +"say")) {
+    message.channel.send(message.content.substring(6);
+}
+
   if (!message.content.startsWith(prefix)) return;
 
   var cmd = client.commands.get(cont[0]);
